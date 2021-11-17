49029bc6f37cea157c8946f8dc3588fcecdffe72
donna <dotdashhenry@gmail.com>
2021-11-17 10:18:37 +0000

    added img and description

--- src/Heading.js
+++ src/Heading.js
@@ -4,8 +4,12 @@ const Heading = () => {
   return (
     <div>
       <header className="App-header">
-        CYF Hotel
-        <img src="./hotel-50.png" />
+        <img
+          className="logo"
+          src="https://cdn-icons-png.flaticon.com/512/594/594106.png"
+          alt="Logo"
+        />
+        <h1>CYF HOTEL</h1>
       </header>
     </div>
   );
